c299de590a4cf1bc0158de78768101e07fbc0ce0
gsell <gselllouise@yahoo.fr>
2019-04-09 11:56:15 +0200

    Les imgaes s'affichent mais la recherche ne fonctionne pas encore

--- Projet_prog_web/app.py
+++ Projet_prog_web/app.py
@@ -9,13 +9,15 @@ from flask import Flask
 from flask import abort, request, make_response
 from flask import render_template, redirect, url_for
 import json
+import re
+from flask import send_from_directory
 # from data import USERS
 # Set API dev in an another file
-from api import SITE_API
+#from api import SITE_API
 
 app = Flask(__name__)
 # Add the API
-app.register_blueprint(SITE_API)
+# app.register_blueprint(SITE_API)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/<identifiant>')
@@ -33,7 +35,7 @@ def index(identifiant=None):
             abort(make_response(error, type_error))
         else:
             identifiant=ID
-            return(fichier) # user= [username, gender, birth , wiki]
+            return render_template('analyses.html') # user= [username, gender, birth , wiki]
     return render_template('index.html')
 
 
@@ -62,43 +64,71 @@ def readfile(nomfichier):
 	content = text.read()
 	text.close()
 	return content
+	
 
-@app.route('/analyses/', methods=['GET', 'POST'])
+def get_infos_analyse(nomdossier):
+    list_fichier=os.listdir("static/data/{nomdossier}/".format(nomdossier=nomdossier))
+    texte=[] # liste contenant le contenu des différents fichiers texte
+    image=[] # liste contenant le lien vers les différents images
+    for item in list_fichier:
+        if item.find(".txt")>=0:
+           app.logger.debug(item)
+           texte.append(readfile("static/data/{nomdossier}/{item}".format(nomdossier=nomdossier, item=item)))
+        elif item.find(".png")>=0:
+            app.logger.debug(item)
+            image.append("../../static/data/{nomdossier}/{image}".format(nomdossier=nomdossier, image=item))
+    return(image,texte)
+
+
+
+@app.route('/analyses/')#, methods=['GET', 'POST'])
 @app.route('/analyses/<nomdossier>/')
 def analyses(nomdossier=None):
     """ Affiche la liste des analyses déjà effectuées ainsi que le contenu (fichier texte+images) des résultats des analyses """
     app.logger.debug('Client request: method:"{0.method}'.format(request))
     if not nomdossier:
-        list_dossier=os.listdir("./data/")
+        list_dossier=os.listdir("static/data/")
         return render_template('analyses.html', repertories=list_dossier)
     else: 
-        list_fichier=os.listdir("./data/{nomdossier}/".format(nomdossier=nomdossier))
-        texte=[] # liste contenant le contenu des différents fichiers texte
-        image=[] # liste contenant le lien vers les différents images
-        for item in list_fichier:
-            if item.find(".txt")>=0:
-                app.logger.debug(item)
-                texte.append(readfile("./data/{nomdossier}/{item}".format(nomdossier=nomdossier, item=item)))
-            elif item.find(".png")>=0:
-                app.logger.debug(item)
-                image.append("./data/{nomdossier}/{image}".format(nomdossier=nomdossier, image=item))
-                #image.append(item)
+        image,texte=get_infos_analyse(nomdossier)
         return render_template('analyses.html',analysis=texte, images=image)
+    return render_template("analyses.html")
 
-#@app.route('/search/', methods=['GET'])
-#def search():
-#    app.logger.debug(request.args)
-#    app.logger.debug('Client request: method:"{0.method}'.format(request))
-#    search = request.args["pattern"]
-#    Name =[]
-#    for user in USERS :
-#         Name.append(user['name'])
-#    if search in Name :
-#        username, gender, birth , wiki = find_user(search)
-#        return render_template('users.html', user= [username, gender, birth , wiki])
-#    else :
-#        resp =  make_response("Not found" , 401)       
-#        return resps
+
+
+def get_info_filtred_data(liste_dossier, pattern):
+    filtered_data=[]
+    exist=False
+    for file_name in liste_dossier :
+        if re.match(pattern, file_name):
+            filtered_names.append(file_name)
+            exist=True
+    if exist:
+        return render_template('analyses.html',filtre=filtered_data) 
+    else:
+        return render_template('analyses.html',error="Pas de nom d'analyse contenant %s."%(pattern))
+        
+        
+def get_info(liste_dossier, pattern):
+    if pattern in liste_dossier:
+        image,texte=get_info_analyse(nomdossier)
+        return render_template('analyses.html',analysis=texte, images=image) 
+    else:
+        return render_template('analyses.html',error="Pas de dossier d'analyse nommé %s."%(pattern)) 
+        #abort(make_response('No user named %s'%(username), 501))
+
+        
+        
+@app.route('/search/', methods=['GET'])
+def search():
+    app.logger.debug(request.args)
+    pattern=request.args["pattern"]
+    if "regexp" in request.args:
+        liste_dossier=os.listdir("static/data/")
+        return get_info_filtred_data(liste_dossier, pattern)
+    else:
+        liste_dossier=os.listdir("static/data/")
+        return get_info(liste_dossier, pattern)
   
     
 
